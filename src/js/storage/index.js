412388e56f227774ba0224fc0e86e0447ea942e5
Fermain <Fermain@users.noreply.github.com>
2022-11-14 13:23:19 +0200

    Adds storage handlers

---
+++ src/js/storage/index.js
@@ -0,0 +1,11 @@
+import { save } from "./save.js";
+import { remove } from "./save.js";
+import { clear } from "./save.js";
+import { load } from "./save.js";
+
+export default {
+  save,
+  remove,
+  clear,
+  load,
+}
